46ecc7e18da60de8b902d839734ec46708ff8abe
Travis-Gray2194 <travisogray1@gmail.com>
2017-10-11 14:41:34 -0400

    Initial commit

---
+++ src/Mathtwo.java
@@ -0,0 +1,13 @@
+public class Mathtwo {
+
+        public static void main(String[] args) {
+            // write your code here
+            int mynumber = 2;
+            double myothernumber = 1.7938;
+
+            System.out.println(mynumber);
+            System.out.println(myothernumber);
+        }
+    }
+
+
